b30e54e0dfa399a8cae1efa795a1e8f9a2d4b49a
safronman <safronmanbox@gmail.com>
2025-07-14 16:42:16 +0300

    upload cover: p.1

--- src/features/playlists/ui/PlaylistsPage/PlaylistItem/PlaylistItem.tsx
+++ src/features/playlists/ui/PlaylistsPage/PlaylistItem/PlaylistItem.tsx
@@ -1,4 +1,6 @@
 import type { Playlist } from '../../../api/playlistsApi.types.ts'
+import defaultCover from '@/assets/images/default-playlist-cover.png'
+import s from './PlaylistItem.module.css'
 
 type Props = {
   playlist: Playlist
@@ -7,8 +9,12 @@ type Props = {
 }
 
 export const PlaylistItem = ({ playlist, editPlaylist, deletePlaylist }: Props) => {
+  const originalCover = playlist.attributes.images.main?.find((img) => img.type === 'original')
+  const src = originalCover ? originalCover?.url : defaultCover
+
   return (
     <div>
+      <img src={src} alt={'cover'} width={'100px'} className={s.cover} />
       <div>title: {playlist.attributes.title}</div>
       <div>description: {playlist.attributes.description}</div>
       <div>userName: {playlist.attributes.user.name}</div>
