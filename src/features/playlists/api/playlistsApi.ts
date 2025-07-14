3479583bffbcddf5ab7325036b10f2316bd711e3
safronman <safronmanbox@gmail.com>
2025-07-14 14:18:44 +0300

    add code splitting

--- src/features/playlists/api/playlistsApi.ts
+++ src/features/playlists/api/playlistsApi.ts
@@ -1,19 +1,7 @@
+import { baseApi } from '@/app/api/baseApi.ts'
 import type { CreatePlaylistArgs, Playlist, PlaylistsResponse, UpdatePlaylistArgs } from './playlistsApi.types.ts'
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-export const playlistsApi = createApi({
-  reducerPath: 'playlistsApi',
-  tagTypes: ['Playlist'],
-  baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_BASE_URL,
-    headers: {
-      'API-KEY': import.meta.env.VITE_API_KEY,
-    },
-    prepareHeaders: (headers) => {
-      headers.set('Authorization', `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`)
-      return headers
-    },
-  }),
+export const playlistsApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
     fetchPlaylists: build.query<PlaylistsResponse, void>({
       query: () => ({ url: `playlists` }),
