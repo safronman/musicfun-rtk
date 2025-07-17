8cd5ee74b4b2467bcbc0593ed9309e5411c9ef90
safronman <safronmanbox@gmail.com>
2025-07-17 11:09:13 +0300

    infinite query: 1

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -5,6 +5,7 @@ import s from './Header.module.css'
 const navItems = [
   { to: Path.Main, label: 'Main' },
   { to: Path.Playlists, label: 'Playlists' },
+  { to: Path.Tracks, label: 'Tracks' },
   { to: Path.Profile, label: 'Profile' },
 ]
 
--- src/common/routing/Routing.tsx
+++ src/common/routing/Routing.tsx
@@ -1,12 +1,14 @@
-import { ProfilePage } from '@/features/auth/ui/ProfilePage/ProfilePage.tsx'
 import { Route, Routes } from 'react-router'
 import { PageNotFound } from '@/common/components'
 import { MainPage } from '@/app/ui/MainPage.tsx'
 import { PlaylistsPage } from '@/features/playlists/ui/PlaylistsPage/PlaylistsPage.tsx'
+import { TracksPage } from '@/features/tracks/ui/TracksPage/TracksPage.tsx'
+import { ProfilePage } from '@/features/auth/ui/ProfilePage/ProfilePage.tsx'
 
 export const Path = {
   Main: '/',
   Playlists: '/playlists',
+  Tracks: '/tracks',
   Profile: '/profile',
   NotFound: '*',
 } as const
@@ -15,6 +17,7 @@ export const Routing = () => (
   <Routes>
     <Route path={Path.Main} element={<MainPage />} />
     <Route path={Path.Playlists} element={<PlaylistsPage />} />
+    <Route path={Path.Tracks} element={<TracksPage />} />
     <Route path={Path.Profile} element={<ProfilePage />} />
     <Route path={Path.NotFound} element={<PageNotFound />} />
   </Routes>
