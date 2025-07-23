33801957a68629b535e0a769a6093bbb9cb0e534
safronman <safronmanbox@gmail.com>
2025-07-23 12:53:37 +0300

    my playlists: 3

--- src/features/auth/ui/ProfilePage/ProfilePage.tsx
+++ src/features/auth/ui/ProfilePage/ProfilePage.tsx
@@ -1,5 +1,7 @@
+import { Path } from '@/common/routing'
 import { useGetMeQuery } from '@/features/auth/api/authApi.ts'
 import { useFetchPlaylistsQuery } from '@/features/playlists/api/playlistsApi.ts'
+import { Navigate } from 'react-router'
 import { CreatePlaylistForm } from '../../../playlists/ui/PlaylistsPage/CreatePlaylistForm/CreatePlaylistForm.tsx'
 import { PlaylistsList } from '../../../playlists/ui/PlaylistsPage/PlaylistsList/PlaylistsList.tsx'
 import s from './ProfilePage.module.css'
@@ -13,6 +15,8 @@ export const ProfilePage = () => {
   )
 
   if (isLoading || isMeLoading) return <h1>Skeleton loader...</h1>
+
+  if (!isMeLoading && !meResponse) return <Navigate to={Path.Playlists} />
 
   return (
     <>
